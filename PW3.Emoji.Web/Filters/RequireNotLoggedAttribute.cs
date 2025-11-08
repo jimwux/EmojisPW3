4d0018bebe5072c5841321e0dc0134a4790a5aae
gonzaviale <gonzaloleonelviale@gmail.com>
2025-11-08 09:21:00 -0300

    feat: :sparkles: add RequireNotLogged filter to restrict access for logged-in users

--- PW3.Emoji.Web/Filters/RequireNotLoggedAttribute.cs
+++ PW3.Emoji.Web/Filters/RequireNotLoggedAttribute.cs
@@ -3,14 +3,14 @@ using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace PW3.Emoji.Web.Filters;
 
-public class RequireLoginAttribute : ActionFilterAttribute
+public class RequireNotLoggedAttribute : ActionFilterAttribute
 {
     public override void OnActionExecuting(ActionExecutingContext context)
     {
         var session = context.HttpContext.Session;
-        if (!session.TryGetValue("UsuarioId", out _))
+        if (session.TryGetValue("UsuarioId", out _))
         {
-            context.Result = new RedirectToActionResult("Login", "Usuario", null);
+            context.Result = new RedirectToActionResult("Analizar", "Emocion", null);
         }
         base.OnActionExecuting(context);
     }
